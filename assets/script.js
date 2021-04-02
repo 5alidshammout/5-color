13ff795a8862a3720de5d8aea729899e03d15045
5alidshammout <5alidshammout@gmail.com>
2021-04-02 16:28:53 +0300

    if(missedAColorThatYouLiked){ openTheConsole() }

--- assets/script.js
+++ assets/script.js
@@ -4,22 +4,23 @@ const bdy = document.querySelector("body");
 let color;
 
 function generateColor() {
-  c = chroma.random();
-  output.innerText = c;
-  bdy.style.backgroundColor = c;
-  if (chroma.contrast(`${c}`, "black") < 4.5) {
-    bdy.style.color = "white";
-    btn.style.color = "white";
-  } else {
-    bdy.style.color = "black";
-    btn.style.color = "black";
-  }
+    c = chroma.random();
+    output.innerText = c;
+    bdy.style.backgroundColor = c;
+    console.log(`%c███████\n${c}`, `font-size:50px;color:${c}`);
+    if (chroma.contrast(`${c}`, "black") < 4.5) {
+        bdy.style.color = "white";
+        btn.style.color = "white";
+    } else {
+        bdy.style.color = "black";
+        btn.style.color = "black";
+    }
 }
 
 btn.onclick = generateColor;
 
 bdy.onkeyup = (e) => {
-  if (e.keyCode == 32) {
-    generateColor();
-  }
+    if (e.keyCode == 32) {
+        generateColor();
+    }
 };
